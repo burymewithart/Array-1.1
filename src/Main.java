d61aa066c38f1d7915893afb66e3262e1257af3a
burymewithart <ar.dautoff@yandex.ru>
2025-09-05 22:58:59 +0300

    Init Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int [] weights = {90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
+        weights[0] = 90;
+        int januaryWeight = weights[0];
+        System.out.println(januaryWeight);
+        System.out.println(weights[0]);
+        System.out.println(weights[4]);
+        int january = 0;
+        System.out.println(weights[january]);
+        for (int i = 0; i < weights.length; i++) {
+            System.out.println(weights[i]);
+        }
+    }
+}
