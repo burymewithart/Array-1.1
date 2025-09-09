358047629875cc2c59fff83006466351476b50dc
burymewithart <ar.dautoff@yandex.ru>
2025-09-09 20:02:31 +0300

    HW Array 1.1 done changed.

--- src/Main.java
+++ src/Main.java
@@ -13,70 +13,8 @@ public class Main {
         for (int i = 0; i < weights.length; i++) {
             System.out.println(weights[i]);
         }
-        {
-            System.out.println("Задача 1");
-            int[] number1 = new int[3];
-            number1[0] = 1;
-            number1[1] = 2;
-            number1[2] = 3;
 
-            double[] number2 = {1.57, 7.654, 9.986};
-
-            int[] array = {10};
-            System.out.println("Целочисленный массив: ");
-            for (int num : number1) {
-                System.out.println(num + " ");
-            }
-            System.out.println("Дробный массив: ");
-            for (double num : number2) {
-                System.out.println(num + " ");
-            }
-            System.out.println("Свой массив: ");
-            for (int num : array) {
-                System.out.println(num + " ");
-            }
-        }
-        {
-            System.out.println("Задача 2");
-            int[] number1 = new int[3];
-            number1[0] = 1;
-            number1[1] = 2;
-            number1[2] = 3;
-
-            double[] number2 = {1.57, 7.654, 9.986};
-
-            int[] array = {10};
-            System.out.println("Целочисленный массив: ");
-            for (int i = 0; i < number1.length; i++) {
-                System.out.print(number1[i]);
-                if (i < number1.length - 1) {
-                    System.out.print(", ");
-                }
-            }
-            System.out.println();
-
-            System.out.println("Дробный массив:");
-            for (int i = 0; i < number2.length; i++) {
-                System.out.print(number2[i]);
-                if (i < number2.length - 1) {
-                    System.out.print(", ");
-                }
-            }
-            System.out.println();
-
-            System.out.println("Свой массив:");
-            for (int i = 0; i < array.length; i++) {
-                System.out.print(array[i]);
-                if (i < array.length - 1) {
-                    System.out.print(", ");
-                }
-            }
-            System.out.println();
-
-        }
-        {
-        System.out.println("Задача 3");
-
+        System.out.println("Задача 1");
         int[] number1 = new int[3];
         number1[0] = 1;
         number1[1] = 2;
@@ -85,7 +23,48 @@ public class Main {
         double[] number2 = {1.57, 7.654, 9.986};
         int[] array = {10};
 
+        System.out.println("Целочисленный массив: ");
+        for (int num : number1) {
+            System.out.println(num + " ");
+        }
+        System.out.println("Дробный массив: ");
+        for (double num : number2) {
+            System.out.println(num + " ");
+        }
+        System.out.println("Свой массив: ");
+        for (int num : array) {
+            System.out.println(num + " ");
+        }
 
+        System.out.println("Задача 2");
+        System.out.println("Целочисленный массив: ");
+        for (int i = 0; i < number1.length; i++) {
+            System.out.print(number1[i]);
+            if (i < number1.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        System.out.println("Дробный массив:");
+        for (int i = 0; i < number2.length; i++) {
+            System.out.print(number2[i]);
+            if (i < number2.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        System.out.println("Свой массив:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        System.out.println("Задача 3");
         System.out.println("Целочисленный массив в обратном порядке:");
         for (int i = number1.length - 1; i >= 0; i--) {
             System.out.print(number1[i]);
@@ -94,7 +73,6 @@ public class Main {
             }
         }
         System.out.println();
-
 
         System.out.println("Дробный массив в обратном порядке:");
         for (int i = number2.length - 1; i >= 0; i--) {
@@ -105,7 +83,6 @@ public class Main {
         }
         System.out.println();
 
-
         System.out.println("Свой массив в обратном порядке:");
         for (int i = array.length - 1; i >= 0; i--) {
             System.out.print(array[i]);
@@ -113,29 +90,21 @@ public class Main {
                 System.out.print(", ");
             }
         }
-    }
         System.out.println();
-        {
-            System.out.println("Задача 4");
-            int[] number1 = new int[3];
-            number1[0] = 1;
-            number1[1] = 2;
-            number1[2] = 3;
 
+        System.out.println("Задача 4");
+        System.out.println("Исходный массив:");
+        for (int i = 0; i < number1.length; i++) {
+            System.out.print(number1[i] + " ");
+        }
+        System.out.println();
 
-            System.out.println("Исходный массив:");
-            for (int i = 0; i < number1.length; i++) {
-                System.out.print(number1[i] + " ");
+        System.out.println("Массив после преобразования:");
+        for (int i = 0; i < number1.length; i++) {
+            if (number1[i] % 2 != 0) {
+                number1[i] += 1;
             }
-            System.out.println();
-
-            System.out.println("Массив после преобразования:");
-            for (int i = 0; i < number1.length; i++) {
-                if (number1[i] % 2 != 0) {
-                    number1[i] += 1;
-                }
-                System.out.print(number1[i] + " ");
-            }
+            System.out.print(number1[i] + " ");
         }
     }
 }
